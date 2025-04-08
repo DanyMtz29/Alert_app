d26fe349eb6daec609125b53786e7497db00535c
DanyMtz29 <danieljm2901@gmail.com>
2025-04-08 01:34:26 -0600

    D-UI's-information: Conflicts solved

--- app/src/main/java/com/example/alert_app/informationActivity.java
+++ app/src/main/java/com/example/alert_app/informationActivity.java
@@ -1,6 +1,8 @@
 package com.example.alert_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class informationActivity extends AppCompatActivity {
 
+    Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informacion);
+
     }
+
+    public void typesViolence(View v){
+        i = new Intent(informationActivity.this, TiposViolencia.class);
+        startActivity(i);
+    }
+
+
 }
